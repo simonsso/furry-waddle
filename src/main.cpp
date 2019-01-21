56185f0ceff7692b33f3cb899b52e0cb0113a126
Fredrik Simonsson <simson@thesimson.net>
2019-01-21 13:42:04 +0900

    Create a curl wrapper and some tests

--- src/main.cpp
+++ src/main.cpp
@@ -4,84 +4,76 @@
 #include <curl/curl.h>
 
 
+//
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
-    return size * nmemb;
+	((std::string*)userp)->append((char*)contents, size * nmemb);
+	return size * nmemb;
 }
 
-/// Create the example with dog and yellow dog.
-// Stupid example but clear to show concept
-class Dog{
-	/// add some mebers
-	int a;		//32 bit
-	int64_t b;	
-	int32_t d;	//32
-	bool e;	bool c;
-	std::string *name;
-
-
+class EasyCurlWrp {
+	CURL *curl= nullptr;
+	std::string readBuffer;
 public:
-	Dog(){
-		name = new std::string("");
-		std::cout << "dog construc"<<std::endl;
+	EasyCurlWrp(){
+		curl = curl_easy_init();
+		if(curl) {
+			curl_easy_setopt(curl, CURLOPT_URL, "http://www.example.com");
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+		}
+	};
+	EasyCurlWrp(char const *url){
+		curl = curl_easy_init();
+		if(curl) {
+			curl_easy_setopt(curl, CURLOPT_URL, url);
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+		}
+	};
+	~EasyCurlWrp(){
+		curl = curl_easy_init();
+		if(curl) {
+			curl_easy_cleanup(curl);
+			curl = nullptr;
+		}
+	};
+	CURLcode perform(){
+		if (curl) {
+			return (curl_easy_perform(curl));
+		}else{
+			return CURLE_FAILED_INIT;
+		}
 	}
-	Dog(Dog& x){
-		(void) x;
-		std::cout << "dog copt construc"<<std::endl;
+	std::string getBuffer(){
+		return readBuffer;
 	}
 
-	virtual ~Dog(){
-		std::cout << "dog destruct his name was "<<name<<std::endl;
-		delete name;
-	};
-	virtual void bark(){
-		std::cout << "dog barking"<<std::endl;
-	}
 };
 
-
-class YellowDog: public Dog{
-	public:
-	YellowDog(){
-		std::cout << "YellowDog construc"<<std::endl;
-	}
-	YellowDog(YellowDog&& x){
-		(void)x;
-		std::cout << "YellowDog MOVE construc"<<std::endl;
-	}
-	YellowDog(Dog&& x){
-		(void)x;
-		std::cout << "YellowDog MOVE2 construc"<<std::endl;
-	}
-	virtual ~YellowDog(){
-		std::cout << "YellowDog destruct"<<std::endl;
-	};
-	virtual void bark() override{
-		std::cout << "Yellowdog barking"<<std::endl;
-	}	
-
-};
 
 
 int main(int argc, char *argv[]) {
 	(void) argc;
 	(void) argv;
 
-	CURL *curl;
-	CURLcode res;
-	std::string readBuffer;
+	EasyCurlWrp backlog("https://blockchain.info/unconfirmed-transactions?format=json");
+	EasyCurlWrp githubio("http://simonsso.github.io");
+	EasyCurlWrp githubioS("https://simonsso.github.io");
 
-	curl = curl_easy_init();
-	if(curl) {
-		curl_easy_setopt(curl, CURLOPT_URL, "https://blockchain.info/unconfirmed-transactions?format=json");
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-		res = curl_easy_perform(curl);
-		curl_easy_cleanup(curl);
-
-		std::cout << readBuffer << std::endl;
+	if (CURLcode res = githubioS.perform() ){
+			std::cout << "Hello Error: " <<res << std::endl;
 	}
+	if (CURLcode res = backlog.perform() ){
+			std::cout << "Hello Error: " <<res << std::endl;
+	}
+	if (CURLcode res = githubio.perform() ){
+			std::cout << "Hello Error: " <<res << std::endl;
+	}
+
+	std::cout << backlog.getBuffer().length()<< std::endl;;
+	std::cout << githubio.getBuffer().length()<< std::endl;;
+	std::cout << githubioS.getBuffer().length()<< std::endl;;
 
 	std::cout << "Hello Easy C++ project!" << std::endl;
 }
