b9245031f4ea8036a6a3c35828f8d653157e7783
Fredrik Simonsson <simson@thesimson.net>
2019-02-02 13:20:47 +0900

    First parsing of data file and filtering on ISIN

--- src/main.cpp
+++ src/main.cpp
@@ -1,106 +1,56 @@
 #include <iostream>
-#include <iostream>
 #include <string>
-#include <curl/curl.h>
 #include "json.hpp"
-//
-static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
-{
-	((std::string*)userp)->append((char*)contents, size * nmemb);
-	return size * nmemb;
-}
-
-class EasyCurlWrp {
-	CURL *curl= nullptr;
-	std::string readBuffer;
-public:
-	EasyCurlWrp(){
-		curl = curl_easy_init();
-		if(curl) {
-			curl_easy_setopt(curl, CURLOPT_URL, "http://www.example.com");
-			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-		}
-	};
-	EasyCurlWrp(char const *url){
-		curl = curl_easy_init();
-		if(curl) {
-			curl_easy_setopt(curl, CURLOPT_URL, url);
-			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-		}
-	};
-	~EasyCurlWrp(){
-		curl = curl_easy_init();
-		if(curl) {
-			curl_easy_cleanup(curl);
-			curl = nullptr;
-		}
-	};
-	CURLcode perform(){
-		if (curl) {
-			return (curl_easy_perform(curl));
-		}else{
-			return CURLE_FAILED_INIT;
-		}
-	}
-	std::string getBuffer(){
-		return readBuffer;
-	}
-
-};
-
-
+#include <fstream>
 using json = nlohmann::json;
 
 int main(int argc, char *argv[]) {
 	(void) argc;
 	(void) argv;
 
-	EasyCurlWrp backlog("https://blockchain.info/unconfirmed-transactions?format=json");
-	EasyCurlWrp githubio("http://simonsso.github.io");
-	EasyCurlWrp githubioS("https://simonsso.github.io");
 
-	if (CURLcode res = githubioS.perform() ){
-			std::cout << "Hello Error: " <<res << std::endl;
-	}
-	if (CURLcode res = backlog.perform() ){
-			std::cout << "Hello Error: " <<res << std::endl;
-	}
-	if (CURLcode res = githubio.perform() ){
-			std::cout << "Hello Error: " <<res << std::endl;
-	}
+	std::ifstream infile( "/home/simson/Documents/Avanza/transaktioner_20150210_20190126.json" );
 
-	std::cout << backlog.getBuffer().length()<< std::endl;;
-	std::cout << githubio.getBuffer().length()<< std::endl;;
-	std::cout << githubioS.getBuffer().length()<< std::endl;;
+	if (infile.is_open()) {
+		try
+		{
+			// parsing input with a syntax error
+			double total_value = 0;
+			auto js = json::parse(infile);
+			if (js.is_array()) {
+				for (auto i : (json::array_t) (js) ){
+					if (i.is_array() ){}
+					else if ( i.is_object() ){
+						auto j = (json::object_t) i;
+						//						std::cout << "ver: "<< j["ver"] << " time "<< j["time"]<< " hash "<<j["hash"] <<std::endl;
+						// Find ISIN field:
+						auto isin_iter = i.find("ISIN");
+						if ( isin_iter != i.end() ){
+							if( std::strcmp("SE0000422107",((std::string) *isin_iter).c_str()) == 0 || 0 == std::strcmp("SE0008374250", ((std::string) *isin_iter).c_str()) ) {
+								auto b = i.find("Belopp") ;
+								if ( b != i.end() ){
+									if ( b->is_number() ) {
+										total_value += (double) *b;
+										std::cout<<":" << *b << ":"<<std::endl ;
+									}else{
+										std::cout << i <<" Something\n";
+									}
+								}
+							}
+						}
+					} else if (i.is_primitive()){
 
-	std::cout << "Hello Easy C++ project!" << std::endl;
-
-     try
-     {
-         // parsing input with a syntax error
-        auto js = json::parse(backlog.getBuffer());
-		auto txs = js.find("txs");
-		if (txs != js.end()){
-			std::cout << "Hellow txs " << txs->is_array() << std::endl;
-		}
-		for (auto i : (json::array_t) (*txs) ){
-		    if (i.is_array() ){}
-			else if ( i.is_object() ){
-				auto j = (json::object_t) i;
-				std::cout << "ver: "<< j["ver"] << " time "<< j["time"]<< " hash "<<j["hash"] <<std::endl;
-			} else if (i.is_primitive()){
-
+					}
+				}
 			}
+			std::cout<< "Total: "<< total_value<<std::endl;
 		}
-	 }
-     catch (json::parse_error& e)
-     {
-         // output exception information
-         std::cout << "message: " << e.what() << '\n'
-                   << "exception id: " << e.id << '\n'
-                   << "byte position of error: " << e.byte << std::endl;
-	 }
-
+		catch (json::parse_error& e)
+		{
+			// output exception information
+			std::cout << "message: " << e.what() << '\n'
+					<< "exception id: " << e.id << '\n'
+					<< "byte position of error: " << e.byte << std::endl;
+		}
+	}
 }
