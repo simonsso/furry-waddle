f1f2994dbdb67695fdf9331bad98053d8f7fb613
Fredrik Simonsson <simson@thesimson.net>
2019-02-18 13:23:31 +0900

    Autoformat and const declarations

--- include/ledger.hpp
+++ include/ledger.hpp
@@ -1,13 +1,13 @@
 #ifndef LEDGER_HPP
 #define LEDGER_HPP
 
+#include <cstdint>
 #include <deque>
 #include <list>
 #include <map>
 #include <mutex>
 #include <string>
 #include <string_view>
-#include <cstdint>
 
 class transaction {
   public:
@@ -30,14 +30,11 @@ class transaction {
    double amount;
    double brokerage;
    uint32_t date;
-   bool operator==(transaction &t) {
+   bool operator==(const transaction &t) const {
       return (t.brokerage == brokerage && t.isin == isin && t.date == date && t.amount == amount && t.curenecy == curenecy &&
               t.sec_name == sec_name);
    }
-   bool operator<(transaction &t) {
-      //	std::cout << "DBG"<< this->date <<" < " <<t.date << std::endl;
-      return this->date < t.date;
-   }
+   bool operator<(const transaction &t) const { return date < t.date; }
 };
 class transaction_set {
   public:
@@ -54,15 +51,16 @@ class Ledger {
    std::map<unsigned int, decltype(ledger.end())> date_index;
 
    mutable std::shared_mutex mutex;
-public:
+
+  public:
    /// Import from csv stream
-   void import_csv(std::istream &infile) ;
+   void import_csv(std::istream &infile);
    /// Verify all asumptionions on data was correct
-   bool data_integrity_self_check() ;
+   bool data_integrity_self_check();
 
    transaction_set sum(const std::string &isin, uint32_t startdate = 0, uint32_t stopdate = 30000000);
    //
-   double april(int startdate, int stopdate) ;
+   double april(int startdate, int stopdate);
    /// Orignal seach code moved into this function.
    void find_something();
 };
